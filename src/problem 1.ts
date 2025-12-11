8d50908278983d3492d52957dd559e4adff09a7b
lthoma39 <lthoma39@uic.edu>
2025-12-11 16:08:32 -0600

    adding possible solution for day1p2

--- src/problem 1.ts
+++ src/problem 1.ts
@@ -5,6 +5,32 @@ interface instruction {
     steps: number;
 }
 
+function crackSecondaryCode(instructions: instruction[]): number {
+
+    let count: number = 0;
+    let position: number = 50;
+    
+    instructions.forEach(
+        (instruction) => {
+            if (instruction.direction === "R") {
+                position = (position + instruction.steps);
+
+                if (position >= 100) { count++ }
+
+                position = position % 100;
+            }
+            else if (instruction.direction === "L") {
+                position = (position - instruction.steps);
+                
+                if (position <= 0) { count++ }
+
+                position = (position + 100) % 100;
+            }
+    });
+
+    return count;
+}
+
 function crackCode(instructions: instruction[]): number {
 
     let count: number = 0;
@@ -40,6 +66,7 @@ function main(): void{
                 }
             );
             console.log(crackCode(instructions));
+            console.log(crackSecondaryCode(instructions));
         }
         catch (err) {
             console.error(err);
