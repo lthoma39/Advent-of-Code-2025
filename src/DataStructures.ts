87b463bb46a9907717b887f1da73e36bc3e1cf3a
lthoma39 <lthoma39@uic.edu>
2025-12-13 17:31:09 -0600

    Preparing grid data structure for problem 4

--- src/DataStructures.ts
+++ src/DataStructures.ts
@@ -8,7 +8,16 @@ interface IStack<T> {
     size(): number;
 }
 
+interface IGrid<T> {
+    getRows(): number;
+    getCols(): number;
+    isOutOfBounds(row: number, col :number): boolean;
+    getCell(row: number, col: number): T | undefined;
+    countMatchingNeighbors(row: number, cel: number, target: T): number;
+}
+
 export class Stack<T> implements IStack<T> {
+    
     private stack: T[] = [];
 
     constructor(){} 
@@ -22,4 +31,32 @@ export class Stack<T> implements IStack<T> {
     pop(): T | undefined { return !this.isEmpty() ? this.stack.pop() : undefined; }
 
     push(val: T): void { this.stack.push(val); }
+}
+
+export class Grid<T> implements IGrid<T> {
+
+    constructor(private grid: T[][], private rows: number, private cols: number) {
+        this.grid = grid;
+    }
+    getRows(): number { return this.rows;}
+    
+    getCols(): number {return this.cols; }
+
+    isOutOfBounds(row: number, col: number): boolean {
+        return false; // WIP
+    }
+
+    getCell(row: number, cel: number): T | undefined { 
+
+        if (!this.isOutOfBounds(row, cel)) { 
+            return this.grid[row][cel]; 
+        }
+        
+        return undefined;
+    }
+
+    countMatchingNeighbors(row: number, cel: number, target: T): number {
+        return 0; // WIP
+    }
+
 }
