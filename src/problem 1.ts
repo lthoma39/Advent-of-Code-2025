4cb97e37c96b47da9b520e0e0efad15b1862918f
Latrell Thomas <lthoma39@uic.edu>
2025-12-13 16:09:48 -0600

    More code cleanup

--- src/problem 1.ts
+++ src/problem 1.ts
@@ -1,11 +1,11 @@
 import fs from 'fs';
 
-interface instruction {
+interface Instruction {
     direction: string;
     steps: number;
 }
 
-function crackSecondaryCode(instructions: instruction[]): number {
+function crackSecondaryCode(instructions: Instruction[]): number {
     let count = 0;
     let position = 50;
 
@@ -15,20 +15,27 @@ function crackSecondaryCode(instructions: instruction[]): number {
         if (inst.direction === "R") {
         
             count += Math.floor((position + steps) / 100);
-          
+
             position = (position + steps) % 100;
         } 
         else { 
+
             count += Math.floor((steps - 1 - position) / 100) + 1;
-           
-            position = (position - steps + 1000) % 100;
+
+            position = (position - steps);
+
+            while (position < 0 && (position * -1) > 100) {
+                position +=  100;
+            }
+
+            position = (position + 100) % 100;
         }
     }
 
     return count;
 }
 
-function crackCode(instructions: instruction[]): number {
+function crackCode(instructions: Instruction[]): number {
 
     let count: number = 0;
     let position: number = 50;
@@ -55,8 +62,8 @@ function main(): void{
             const instructionOrders: string[] = fs.readFileSync('input1.txt', 'utf8').split('\n');
             const instructionOrders2: string[] = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"];
             
-            let instructions: instruction[] = instructionOrders.map(
-                (instruction): instruction => {
+            let instructions: Instruction[] = instructionOrders.map(
+                (instruction): Instruction => {
                     return {
                         direction: instruction.charAt(0),
                         steps: parseInt(instruction.slice(1))
@@ -64,8 +71,8 @@ function main(): void{
                 }
             );
 
-            let instructions2: instruction[] = instructionOrders2.map(
-                (instruction): instruction => {
+            let instructions2: Instruction[] = instructionOrders2.map(
+                (instruction): Instruction => {
                     return {
                         direction: instruction.charAt(0),
                         steps: parseInt(instruction.slice(1))
