3ab7b965783cd50409b57651e769ef918ca36909
lthoma39 <lthoma39@uic.edu>
2025-12-14 14:49:11 -0600

    pushing solution for Q4P2

--- src/problem 4.ts
+++ src/problem 4.ts
@@ -1,19 +1,33 @@
 import { Grid } from './DataStructures';
 
+function checkAislesForTP(grid: Grid): [number, number][] {
+
+    let coordinates: [number, number][] = [];
+
+    for (let x = 0; x < grid.getRows(); x++){
+        for (let y = 0; y < grid.getCols(); y++){
+            if (grid.getCell(x, y) === '@' && grid.countMatchingNeighbors(x, y, '@') < 4){
+                coordinates.push([x, y]);
+            }
+        }
+    }
+
+    return coordinates;
+}
+
 function main(): void {
 
     let grid: Grid = new Grid('../data/input4.txt');
 
+    let availableTP: [number, number][] = checkAislesForTP(grid);
     let toiletPaper: number = 0;
 
-    for (let x = 0; x < grid.getRows(); x++){
-        for (let y = 0; y < grid.getCols(); y++){
-
-            if (grid.getCell(x, y) === '@' && grid.countMatchingNeighbors(x, y, '@') < 4){
-                toiletPaper++;
-                //grid.markCell(x, y); //check neighbors for updates...
-            }
+    while (availableTP.length !== 0){
+        for (const [x, y] of availableTP){
+            toiletPaper += 1;
+            grid.markCell(x, y);
         }
+        availableTP = checkAislesForTP(grid);
     }
 
     console.log(toiletPaper);
