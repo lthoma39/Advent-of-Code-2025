f6dcc24f47a5da5f8ab9e1c8e87fbf8bc5cfd88f
lthoma39 <lthoma39@uic.edu>
2025-12-14 15:07:37 -0600

    changed solution again bc i figured out how to make it similar to my initial approach

--- src/problem 4.ts
+++ src/problem 4.ts
@@ -1,33 +1,25 @@
 import { Grid } from './DataStructures';
 
-function checkAislesForTP(grid: Grid): [number, number][] {
-
-    let coordinates: [number, number][] = [];
-
-    for (let x = 0; x < grid.getRows(); x++){
-        for (let y = 0; y < grid.getCols(); y++){
-            if (grid.getCell(x, y) === '@' && grid.countMatchingNeighbors(x, y, '@') < 4){
-                coordinates.push([x, y]);
-            }
-        }
-    }
-
-    return coordinates;
-}
-
 function main(): void {
 
     let grid: Grid = new Grid('../data/input4.txt');
 
-    let availableTP: [number, number][] = checkAislesForTP(grid);
+    let availableTP: boolean = true;
     let toiletPaper: number = 0;
+    
+    while (availableTP){
 
-    while (availableTP.length !== 0){
-        for (const [x, y] of availableTP){
-            toiletPaper += 1;
-            grid.markCell(x, y);
+        availableTP = false;
+
+        for (let x = 0; x < grid.getRows(); x++){
+            for (let y = 0; y < grid.getCols(); y++){
+                if (grid.getCell(x, y) === '@' && grid.countMatchingNeighbors(x, y, '@') < 4){
+                    toiletPaper += 1;
+                    grid.markCell(x, y);
+                    availableTP = true;
+                }
+            }
         }
-        availableTP = checkAislesForTP(grid);
     }
 
     console.log(toiletPaper);
