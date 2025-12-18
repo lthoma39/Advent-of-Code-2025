710abb826ccc8de44e15fb8988bd19deb0cfa177
lthoma39 <lthoma39@uic.edu>
2025-12-17 19:03:23 -0600

    working on part 2 still

--- src/problem 7.ts
+++ src/problem 7.ts
@@ -24,9 +24,60 @@ class GridGraph extends Grid {
         console.log(this.grid);
     }
 
-    countSplits(numSplits: {value: number}): number {
+    countSplits(isTimelineCount: boolean = false): number {
 
-        return 0;
+        let numSplits: number = 0;
+        let numTimelines: number = 0;
+        let currPosition: Point;
+        let x, y: number;
+
+        while(!this.q.isEmpty()){
+            currPosition = this.q.pop() as Point;
+            x = currPosition[0];
+            y = currPosition[1];
+
+            let endSearch = false;
+
+            while(!this.isOutOfBounds(x, y) && !endSearch){
+                
+                let currCell: string = this.getCell(x, y) as string; 
+
+
+                if (currCell === 'S' || currCell === '.' || currCell === '|'){          
+                    this.markCell(x, y, '|');
+                    x++;
+                }
+                else if (currCell === '^'){
+                    numSplits++;
+                    //we split                
+                    if (!this.isOutOfBounds(x, y - 1)) this.q.add([x, y - 1]);
+                    if (!this.isOutOfBounds(x, y + 1)) this.q.add([x, y + 1]);
+                    endSearch = true;   
+                }
+                else { //its been discovered already 
+                    endSearch = true;
+                }
+            }
+
+            // if (x === this.getRows() && y >= 0 && y <= this.getCols() - 1){
+            //         numTimelines++;
+            //         this.markCell(x, y, 'x');
+
+            //         while (true){
+            //             let reverseCell: string = this.getCell(x - 1, y) as string; 
+
+            //             if (reverseCell === '|'){
+            //                 this.markCell(x - 1, y, 'x');
+            //             }
+            //             else if (reverseCell === '^'){
+                            
+            //             }
+
+            //         }
+            // }
+        }
+
+        return isTimelineCount ? numTimelines : numSplits;
     }
 }
 
@@ -34,11 +85,7 @@ function main(): void {
 
     let grid: GridGraph = new GridGraph('../data/input7.txt', "\\r?\\n", true);
 
-    grid.displayGrid();
-    
-    // const result: {value: number} = {value: 0};
-
-    // console.log(grid);
+    console.log(grid.countSplits(true));
 }
 
 main();
