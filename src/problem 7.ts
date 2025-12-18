9bce564511afe2f77097b8b22c0c615b7c2307a0
lthoma39 <lthoma39@uic.edu>
2025-12-18 12:25:17 -0600

    Finally finished day 7

--- src/problem 7.ts
+++ src/problem 7.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { Grid, Queue } from './DataStructures';
+import { count } from 'console';
 
 type Point = [x: number, y: number];
 
@@ -7,6 +8,8 @@ type Point = [x: number, y: number];
 class GridGraph extends Grid {
 
     private q: Queue<Point> = new Queue<Point>();
+    private startingPoint: Point = [0, 0];
+    private visited: Map<string, number> = new Map<string, number>();
 
     constructor(filepath: string, splitter: string = '\n', isRegEx: boolean = false){
 
@@ -14,14 +17,46 @@ class GridGraph extends Grid {
 
         for (let col = 0; col < this.grid[0].length; col++){
             if (this.grid[0][col] === 'S'){
-                this.q.add([0, col]);
+                const point: Point = [0, col];
+                this.startingPoint = point;
+                this.q.add(point);
                 break;
             }
         }
     }
 
+    convertToKey(x: number, y:number): string { 
+        return `${x},${y}`;
+    }
+
     displayGrid(): void {
         console.log(this.grid);
+    }
+
+    countTimelines(x: number = this.startingPoint[0], y: number = this.startingPoint[1]): number {
+
+        if (this.isOutOfBounds(x, y)) return 0;
+
+        if (x === this.getRows() - 1) return 1;
+        
+        const key = this.convertToKey(x, y);
+        
+        if (this.visited.has(key)) return this.visited.get(key)!;
+
+        let result = 0;
+        let cell: string = this.getCell(x,y)!;
+
+        if (cell === 'S' ||cell === '.'){
+            result = this.countTimelines(x + 1, y);
+        }
+
+        else if (cell === '^'){
+            //we split
+            result = this.countTimelines(x, y - 1) + this.countTimelines(x, y + 1); 
+        }
+
+        this.visited.set(key, result);
+        return result; 
     }
 
     countSplits(): number {
@@ -66,7 +101,8 @@ function main(): void {
 
     let grid: GridGraph = new GridGraph('../data/input7.txt', "\\r?\\n", true);
 
-    console.log(grid.countSplits());
+    // console.log(grid.countSplits());
+    console.log(grid.countTimelines());
 }
 
 main();
