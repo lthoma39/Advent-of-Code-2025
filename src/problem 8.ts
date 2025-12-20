7e4bb3cb87f3ca555bcc179d21dee69a0974116e
lthoma39 <lthoma39@uic.edu>
2025-12-20 15:43:41 -0600

    Did code refactoring

--- src/problem 8.ts
+++ src/problem 8.ts
@@ -2,86 +2,104 @@ import fs from 'fs';
 import { Queue } from './DataStructures';
 
 type PointPairs = {
-    pointA: Point3D;
-    pointB: Point3D;
+    pointA: Node;
+    pointB: Node;
     distance: number;
 }
 
-class Point3D { 
+class Node { 
     public x: number;
     public y: number;
     public z: number;
-    public neighbors: Queue<Point3D>;
+    public neighbors: Node[];
 
     constructor(x: number, y: number, z: number){
         this.x = x;
         this.y = y;
         this.z = z;
-        this.neighbors = new Queue<Point3D>();
+        this.neighbors = [];
+    }
+}
+
+class Graph {
+    private junctionBoxes: Node[];
+    private groups: number;
+    private visited: Set<string>;
+
+    constructor(filepath: string[]){
+        this.junctionBoxes = [];
+        this.groups = 0;
+        this.visited = new Set<string>();
+
+        this.buildJunctionBox(filepath);
     }
 
-    convertToKey(): string {
-        return `${this.x},${this.y},${this.z}`;
+    private convertToKey(n: Node): string {
+        return `${n.x},${n.y},${n.z}`;
     }
 
-    euclideanDistance(point: Point3D): number {
-        return (Math.sqrt((Math.pow((this.x - point.x), 2)) + (Math.pow((this.y - point.y), 2)) + (Math.pow((this.z - point.z), 2))));
+    private euclideanDistance(nodeA: Node, nodeB: Node): number {
+        return (Math.sqrt((Math.pow((nodeA.x - nodeB.x), 2)) + (Math.pow((nodeA.y - nodeB.y), 2)) + (Math.pow((nodeA.z - nodeB.z), 2))));
     }
 
-    countPathLength(): number {
-        return 0;
+    private buildJunctionBox (lines: String[]): void { 
+        for (const line of lines){
+            let coordinates: string[] = line.split(",");
+    
+            if (coordinates.length !== 3){
+                console.log("File read failed");
+                return;
+            }
+            else {
+                this.junctionBoxes.push(new Node(Number(coordinates[0]), Number(coordinates[1]), Number(coordinates[2])));
+            }
+        }
+    }
+
+    private sortJunctionBoxes(): PointPairs[] {
+        let junctionDistance: PointPairs[] = [];
+
+        for (let i = 0; i < this.junctionBoxes.length - 1; i++){
+            const pointA: Node = this.junctionBoxes[i];
+            for (let j = i + 1; j < this.junctionBoxes.length; j++){
+                const pointB: Node = this.junctionBoxes[j];
+
+                junctionDistance.push(
+                    { 
+                        pointA: pointA,
+                        pointB: pointB,
+                        distance: this.euclideanDistance(pointA, pointB) 
+                    }
+                );
+            }
+        }
+
+        junctionDistance.sort(
+            (pairA: PointPairs, pairB: PointPairs) => {
+                return pairA.distance - pairB.distance;
+            }
+        );
+
+        return junctionDistance;
+    }
+
+    private buildGraph(){
+
+        let weightedNodes: PointPairs[] = this.sortJunctionBoxes();
+
+    }
+
+    public countPathLengths(): number[] {
+        return [];
     }
 }
 
 function main(): void{ 
-    
-    let data = fs.readFileSync('../data/input8.txt', 'utf8').split(new RegExp("\\r?\\n")).map (line => line.trim());
-    let junctionBoxes: Point3D[] = [];
-    let junctionDistance: PointPairs[] = [];
-    let visitedNodes: Set<Point3D> = new Set<Point3D>();
+    let graph: Graph = new Graph(fs.readFileSync('../data/input8.txt', 'utf8').split(new RegExp("\\r?\\n")).map (line => line.trim()));
 
-    for (const line of data){
-        let coordinates: string[] = line.split(",");
+    let results = graph.countPathLengths();
 
-        if (coordinates.length !== 3){
-            console.log("File read failed");
-            return;
-        }
-        else {
-            junctionBoxes.push(new Point3D(Number(coordinates[0]), Number(coordinates[1]), Number(coordinates[2])));
-        }
-    }
-
-    for (let i = 0; i < junctionBoxes.length - 1; i++){
-        const pointA: Point3D = junctionBoxes[i];
-        for (let j = i + 1; j < junctionBoxes.length; j++){
-            const pointB: Point3D = junctionBoxes[j];
-            let distance: number = pointA.euclideanDistance(pointB);
-
-            junctionDistance.push(
-                { 
-                    pointA: pointA,
-                    pointB: pointB,
-                    distance: pointA.euclideanDistance(pointB) 
-                }
-            );
-        }
-    }
-
-    junctionDistance.sort(
-        (pairA: PointPairs, pairB: PointPairs) => {
-            return pairA.distance - pairB.distance;
-        }
-    );
-
-    for (let i = 0; i < junctionDistance.length; i++){
-        let pairs: PointPairs = junctionDistance[i];
-        const pairA: Point3D = pairs.pointA;
-        const pairB: Point3D = pairs.pointB;
-
-        pairA.neighbors.add(pairB);
-        pairB.neighbors.add(pairA);
-    }
+    console.log(results);
 }
 
 main();
